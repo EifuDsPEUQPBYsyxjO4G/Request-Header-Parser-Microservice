cf7ecf8d8d7ee5369ee11d800897cf2b5fb1722a
EifuDsPEUQPBYsyxjO4G <liam.cottam1@gmail.com>
2017-07-23 11:01:22 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/whoami', (req, res) => {
+    var useragent = req.headers['user-agent'];
+
+    res.json({
+        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        language: req.headers['accept-language'].split(',')[0],
+        software: useragent.substring(useragent.indexOf('(') + 1, useragent.indexOf(')')),
+    });
+});
+
+app.listen(process.env.PORT || 3000);
