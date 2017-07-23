6c027455405f015797ddc29bc49b481fcb58ad09
EifuDsPEUQPBYsyxjO4G <30383424+EifuDsPEUQPBYsyxjO4G@users.noreply.github.com>
2017-07-23 13:22:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/api/whoami', (req, res) => {
     var useragent = req.headers['user-agent'];
 
     res.json({
-        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        ipaddress: req.headers['x-forwarded-for'].split(',')[0] || req.connection.remoteAddress,
         language: req.headers['accept-language'].split(',')[0],
         software: useragent.substring(useragent.indexOf('(') + 1, useragent.indexOf(')')),
     });
